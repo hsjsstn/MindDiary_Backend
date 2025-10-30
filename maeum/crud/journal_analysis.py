2f1c1dd53bf40561ca71139d78e932463e321342
bangjiwoo <bangjiwoo007722@gmail.com>
2025-10-31 04:51:59 +0900

    [fix] 함수 이름 수정

--- maeum/crud/journal_analysis.py
+++ maeum/crud/journal_analysis.py
@@ -1,5 +1,5 @@
-from sqlalchemy.orm import Session
-from sqlalchemy import extract
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import extract, select, and_
 import datetime
 from typing import List, Tuple
 from uuid import UUID
@@ -8,8 +8,8 @@ import uuid
 from maeum.database.models import JournalAnalysis, JournalEntry
 from maeum.schemas.ai import ReportRes
 
-def create_journal_analysis(db: Session, journal_id: UUID, data: ReportRes):
-    """JournalAnalysis 테이블에 새 분석 결과 저장"""
+def create_journal_analysis(db: AsyncSession, journal_id: UUID, data: ReportRes):
+    # """JournalAnalysis 테이블에 새 분석 결과 저장"""
     emotions = data.emotions.dict()
 
     mapping = {
@@ -33,20 +33,39 @@ def create_journal_analysis(db: Session, journal_id: UUID, data: ReportRes):
 
     return new_analysis
 
-def read_analysis_by_month(
-        db: Session,
+async def read_analysis_by_month(
+        db: AsyncSession,
         user_id: UUID,
         year: int,
         month: int
 ) -> List[JournalAnalysis]:
     # star, end = month_range(year, month)
-    analysis = (
+    stmt = (
         db.query(JournalAnalysis)
-        .join(JournalEntry, JournalEntry.journal_id == JournalEntry.journal_id)
-        .filter(JournalEntry.user_id == user_id)
-        .filter(extract('year', JournalAnalysis.date) == year)
-        .filter(extract('month', JournalAnalysis.date) == month)
+        .join(JournalEntry, JournalAnalysis.journal_id == JournalEntry.journal_id)
+        .where(JournalEntry.user_id == user_id)
+        .where(extract('year', JournalAnalysis.date) == year)
+        .where(extract('month', JournalAnalysis.date) == month)
         .all()
     )
+    res = await db.execute(stmt)
 
-    return analysis
+    return res.scalars().all()
+
+async def read_analysis_recent_n(
+        db: AsyncSession,
+        user_id: UUID,
+        n: int = 7,
+) -> List[JournalAnalysis]:
+    
+    query = (
+        select(JournalAnalysis)
+        .join(JournalEntry, JournalAnalysis.journal_id==JournalEntry.journal_id)
+        .where(JournalEntry.user_id == user_id)
+        .order_by(JournalAnalysis.date.desc(), JournalAnalysis.analysis_id.desc())
+        .limit(n)   
+    )
+    res = await db.execute(query)
+    rows = res.scalars().all()
+
+    return list(reversed(rows))
